67e3e1ebbcbb21040d7823aaffed78575c1a944c
Avinash <>
2016-11-22 15:39:14 +0530

    Spring Test sample (netbeans project)

---
+++ spring-test-example/src/main/java/com/example/dao/PersonDao.java
@@ -0,0 +1,11 @@
+package com.example.dao;
+
+import com.example.model.Person;
+
+public interface PersonDao {
+    
+    public Person findById(Long id);
+    public void create(Person person);
+    public void update(Person person);
+    public void delete(Long id);
+}
