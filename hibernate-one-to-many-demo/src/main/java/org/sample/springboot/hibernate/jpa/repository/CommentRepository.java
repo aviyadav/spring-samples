fe2a371b3528fddb34042cdb64ea2db5e5f286a3
Avinash Yadav <aviyadav@users.noreply.github.com>
2018-11-13 18:48:11 +0530

    Spring hibernate one to many
    Spring hibernate one to many

---
+++ hibernate-one-to-many-demo/src/main/java/org/sample/springboot/hibernate/jpa/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package org.sample.springboot.hibernate.jpa.repository;
+
+import org.sample.springboot.hibernate.jpa.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByPostId(Long postId, Pageable pageable);
+}
